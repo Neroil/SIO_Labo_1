659f47efbc52983ddc98ca542ca77760224b1898
neroil <edwin.haffner@heig-vd.ch>
2024-10-11 10:43:19 +0200

    Changed group name and commented FlatLightLaf that doesn't EXIST

--- src/main/java/ch/heig/sio/lab1/groupD/Gui.java
+++ src/main/java/ch/heig/sio/lab1/groupD/Gui.java
@@ -1,9 +1,9 @@
-package ch.heig.sio.lab1.groupX;
+package ch.heig.sio.lab1.groupD;
 
 import ch.heig.sio.lab1.display.HeuristicComboItem;
 import ch.heig.sio.lab1.display.TspSolverGui;
 import ch.heig.sio.lab1.sample.CanonicalTour;
-import com.formdev.flatlaf.FlatLightLaf;
+//import com.formdev.flatlaf.FlatLightLaf;
 
 public final class Gui {
   public static void main(String[] args) {
@@ -15,7 +15,7 @@ public final class Gui {
     // May not work on all platforms, comment out if necessary
     System.setProperty("sun.java2d.opengl", "true");
 
-    FlatLightLaf.setup();
+    //FlatLightLaf.setup();
     new TspSolverGui(1400, 800, "TSP solver", heuristics);
   }
 }
